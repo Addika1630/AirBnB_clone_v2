a42452c0d20646c8615fc8761fec443aa543c53d
Addika1630 <addika1630@gmail.com>
2023-07-09 06:38:20 -0700

    AirBnB deploy

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,20 +3,28 @@
 Fabric script that generates a tgz archive from the contents of the web_static
 folder of the AirBnB Clone repo
 """
-
-from datetime import datetime
+import os
 from fabric.api import local
-from os.path import isdir
-
+from datetime import datetime
 
 def do_pack():
-    """generates a tgz archive"""
+    """Generate a .tgz archive from the contents of the web_static folder."""
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d%H%M%S")
+
+    # Create the versions folder if it doesn't exist
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+
+    # Create the archive file name
+    archive_name = "web_static_" + timestamp + ".tgz"
+
     try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static/".format(file_name))
-        return file_name
-    except:
+        # Compress the web_static folder into the archive
+        local("tar -cvzf versions/{} web_static".format(archive_name))
+        archive_path = "versions/{}".format(archive_name)
+        print("web_static packed: {} -> {} Bytes".format(archive_path, os.path.getsize(archive_path)))
+        return archive_path
+    except Exception as e:
+        print("Packaging failed: {}".format(e))
         return None
