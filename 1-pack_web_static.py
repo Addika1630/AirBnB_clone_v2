5d271ddb492e3922c7ab6ced400c37a82591984a
Addika1630 <addika1630@gmail.com>
2023-07-09 05:03:28 -0700

    AirBnB deploy

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -16,7 +16,7 @@ def do_pack():
         if isdir("versions") is False:
             local("mkdir versions")
         file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
+        local("tar -cvzf {} web_static/".format(file_name))
         return file_name
     except:
         return None
